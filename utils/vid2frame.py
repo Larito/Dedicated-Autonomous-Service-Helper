ca7a4357f6324aff960761c1ea7d3d9ea1ed13a1
Lara <lara13otaibi@gmail.com>
2024-02-12 01:20:21 +0300

    uploadv2

--- utils/vid2frame.py
+++ utils/vid2frame.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 import cv2
 
-data_path = "/Users/laraalotaibi/Desktop/Archive/videos"
-saved_path= "/Users/laraalotaibi/Desktop/Archive/save"
+data_path = "/Users/lara/Desktop/DASH/data/Archive/videos"
+saved_path= "/Users/lara/Desktop/DASH/data/Archive/save"
 video_paths = os.listdir(data_path)
 
 for video_name in video_paths:
@@ -19,26 +19,17 @@ for video_name in video_paths:
             ret, frame = video.read()
         if not ret: break
 
-        height, width = frame.shape[:2]
-
-        height_factor = 1.5
-        src_points = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
-        dst_points = np.float32([[0, 0], [width, 0], [width, int(height * height_factor)], [0, int(height * height_factor)]])
-
-        perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-        warped = cv2.warpPerspective(frame, perspective_matrix, (width, int(height * height_factor)))
-
         cv2.imshow("frame", frame)
-        cv2.imshow("warped", warped)
+        
         key = cv2.waitKey(0)
 
         if key == ord("q"): exit()
         if key == ord("k"): break
         if key == ord("s"): 
             frame_count += 1
-            cv2.imwrite(saved_path, f"frame{frame_count}.jpg", frame)
-            cv2.imwrite(saved_path, f"frame{frame_count}_rotated.jpg", warped)
-            read_next_frame = False
+            vid_name=video_name[:-4]
+            file_path = os.path.join(saved_path, f"frame{frame_count}_{vid_name}.jpg")
+            cv2.imwrite(file_path, frame)
 
         if key == ord("r"):
             frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
